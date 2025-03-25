ad48b9ecdbf646573c8f405732d4f7a7a159e4ce
Gabriel Pauletti Egea <ga.paulettiegea@gmail.com>
2025-03-25 15:57:16 -0300

    feat(main): adicionado seção nossa plataforma e pra quem é coding quest

--- src/large/pages/InitialScreen/InitialScreen.tsx
+++ src/large/pages/InitialScreen/InitialScreen.tsx
@@ -3,8 +3,6 @@ import { useState, useEffect } from "react";
 import Button from "../../components/button";
 import Logo from "../../components/logo";
 import SliderItem from "../../components/slider/SliderItem";
-
-import "../../customClasses.css";
 import JavaScriptIcon from "../../components/icons/iconsInitialScreen/jsIcon";
 import PythonIcon from "../../components/icons/iconsInitialScreen/PythonIcon";
 import JavaIcon from "../../components/icons/iconsInitialScreen/JavaIcon";
@@ -12,6 +10,16 @@ import PhpIcon from "../../components/icons/iconsInitialScreen/PhpIcon";
 import DartIcon from "../../components/icons/iconsInitialScreen/DartIcon";
 import CplusplusIcon from "../../components/icons/iconsInitialScreen/CplusplusIcon";
 import RubyIcon from "../../components/icons/iconsInitialScreen/RubyIcon";
+
+import dailyChal from "../../assets/icons/iconsInitialScreen/dailyChal.png";
+import mapColored from "../../assets/icons/iconsInitialScreen/mapColored.png";
+import learn from "../../assets/icons/iconsInitialScreen/learn.png";
+import trophy from "../../assets/icons/iconsInitialScreen/trophy.png";
+import firstMedal from "../../assets/icons/iconsInitialScreen/firstPositionMedal.png";
+import secondMedal from "../../assets/icons/iconsInitialScreen/secondPositionMedal.png";
+import thirdMedal from "../../assets/icons/iconsInitialScreen/thirdPositionMedal.png";
+
+import "../../customClasses.css";
 type TypingTextProps = {
   text: string;
   speed?: number;
@@ -59,6 +67,8 @@ export default function InitialScreen() {
             </button>
           </div>
         </div>
+        {/* ------- */}
+        {/* Main */}
         <div className="flex flex-col w-screen h-max items-center gap-40">
           {/* InitialText */}
           <div className="flex flex-col items-center gap-10">
@@ -241,6 +251,7 @@ export default function InitialScreen() {
         </div>
       </div>
 
+      {/* O que é coding quest */}
       <div className="flex justify-center items-center gap-10 h-[60vh] w-screen">
         <Logo size={450} />
         <div className="flex flex-col gap-10 w-2xl ">
@@ -268,29 +279,132 @@ export default function InitialScreen() {
         </div>
       </div>
 
-      {/* <div className="flex flex-col">
-        <h2 className="primaryColor text-3xl">Nossa plataforma</h2>
+      {/* Nossa plataforma */}
+      <div className="flex flex-col w-screen h-[90vh] justify-center items-center">
+        <div className="flex flex-col items-start justify-center gap-15">
+          <h2 className="primaryColor text-4xl font-bold">Nossa plataforma</h2>
 
-        <div className="flex ">
-          <div>
-            <div>
-              <span></span>
-              <figure></figure>
-              <span></span>
+          <div className="flex justify-center items-center gap-20 h-max">
+            <div className="flex flex-col gap-15 w-auto">
+              <div className="flex items-center gap-20">
+                {/* Card Desafios Diários */}
+                <div className="flex flex-col items-center gap-5 pt-5 pb-5 bg-[#3c355a] border-3 rounded-3xl border-[#24C97B] w-90 h-90">
+                  <span className="primaryColor font-semibold text-2xl">
+                    Desafios Diários
+                  </span>
+                  <img className="w-30" src={dailyChal} alt="" />
+                  <span className="primaryColor w-[85%] text-center">
+                    Lorem ipsum cras proin venenatis proin dictumst lacinia
+                    ornare malesuada mollis, at pulvinar nec etiam curae
+                    pulvinar lorem primis nibh, nostra nisl nunc litora aliquam
+                    cras ipsum mauris laoreet.
+                  </span>
+                </div>
+
+                {/* Card Modo Campanha*/}
+                <div className="flex flex-col items-center gap-5 pt-5 pb-5 bg-[#3c355a] border-3 rounded-3xl border-[#24C97B] w-90 h-90">
+                  <span className="primaryColor font-semibold text-2xl">
+                    Modo Campanha
+                  </span>
+                  <img className="w-30" src={mapColored} alt="" />
+                  <span className="primaryColor w-[85%] text-center">
+                    Lorem ipsum cras proin venenatis proin dictumst lacinia
+                    ornare malesuada mollis, at pulvinar nec etiam curae
+                    pulvinar lorem primis nibh, nostra nisl nunc litora aliquam
+                    cras ipsum mauris laoreet.
+                  </span>
+                </div>
+              </div>
+
+              <div className="flex gap-5 pt-5 pb-5 bg-[#3c355a] border-3 rounded-3xl border-[#24C97B] w-200 h-max">
+                <div className="flex h-max w-[90%] justify-center items-center">
+                  <div className="flex flex-col items-center justify-center gap-3">
+                    <span className="primaryColor font-semibold text-2xl w-[85%]">
+                      Aprendizado
+                    </span>
+                    <span className="primaryColor w-[85%]">
+                      Lorem ipsum cras proin venenatis proin dictumst lacinia
+                      ornare malesuada mollis, at pulvinar nec etiam curae
+                      pulvinar lorem primis nibh, nostra nisl nunc litora
+                      aliquam cras ipsum mauris laoreet. Lorem ipsum cras proin
+                      venenatis proin dictumst lacinia ornare malesuada.
+                    </span>
+                  </div>
+                  <img className="w-30 h-30" src={learn} alt="" />
+                </div>
+              </div>
             </div>
 
-            <div>
-            <span></span>
-              <figure></figure>
-              <span></span>
+            <div className="flex flex-col items-center gap-5 pt-5 pb-5 bg-[#3c355a] border-3 rounded-3xl border-[#24C97B] w-90 h-151">
+              <span className="primaryColor font-semibold text-2xl">
+                Conquistas
+              </span>
+              <img className="w-60" src={trophy} alt="" />
+              <span className="primaryColor text-center w-[85%]">
+                Lorem ipsum cras proin venenatis proin dictumst lacinia ornare
+                malesuada mollis, at pulvinar nec etiam curae pulvinar lorem
+                primis nibh, nostra nisl nunc litora aliquam cras ipsum mauris
+                laoreet. consectetur laoreet rutrum eu libero et facilisis
+                suspendisse, tellus luctus ornare habitasse etiam massa
+                pellentesque aliquet, viverra nunc leo suspendisse nunc
+              </span>
             </div>
-
-            <div></div>
           </div>
-
-          <div></div>
         </div>
-      </div> */}
+      </div>
+
+      {/* Pra quem é */}
+      <div className="flex w-screen h-[80vh] justify-center items-center">
+        <div className="flex w-max h-max flex-col justify-center items-center gap-30">
+          <h2 className="primaryColor text-4xl font-bold">
+            Pra quem é a Coding Quest?
+          </h2>
+          <div className="flex gap-25 items-center justify-center">
+            <div className="flex flex-col items-center gap-5 pt-5 pb-5 bg-[#3c355a] border-3 rounded-3xl border-[#24C97B] w-80 h-100">
+              <img className="relative -top-6.5" src={thirdMedal} alt="" />
+              <span className="primaryColor font-semibold text-xl w-[85%] text-center">
+                Quem está querendo começar a Estudar!
+              </span>
+              <span className="primaryColor w-[85%] text-center">
+                Lorem ipsum cras proin venenatis proin dictumst lacinia ornare
+                malesuada mollis, at pulvinar nec etiam curae pulvinar lorem
+                primis nibh, nostra nisl nunc litora aliquam cras ipsum mauris
+                laoreet.
+              </span>
+            </div>
+            <div className="flex flex-col items-center gap-5 pt-5 pb-5 bg-[#3c355a] border-3 rounded-3xl border-[#24C97B] w-80 h-100">
+              <img className="relative -top-5" src={secondMedal} alt="" />
+              <span className="primaryColor font-semibold text-xl w-[85%] text-center">
+                Quem está estudando programação!
+              </span>
+              <span className="primaryColor w-[85%] text-center">
+                Lorem ipsum cras proin venenatis proin dictumst lacinia ornare
+                malesuada mollis, at pulvinar nec etiam curae pulvinar lorem
+                primis nibh, nostra nisl nunc litora aliquam cras ipsum mauris
+                laoreet.
+              </span>
+            </div>
+            <div className="flex flex-col items-center gap-5 pt-5 pb-5 bg-[#3c355a] border-3 rounded-3xl border-[#24C97B] w-80 h-100">
+              <img className="relative -top-5" src={firstMedal} alt="" />
+              <span className="primaryColor font-semibold text-xl w-[85%] text-center">
+                Quem já sabe programar!
+              </span>
+              <span className="primaryColor w-[85%] text-center">
+                Lorem ipsum cras proin venenatis proin dictumst lacinia ornare
+                malesuada mollis, at pulvinar nec etiam curae pulvinar lorem
+                primis nibh, nostra nisl nunc litora aliquam cras ipsum mauris
+                laoreet.
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* ------ */}
+      {/* Footer */}
+
+      <div className="flex justify-center items-center w-screen h-70 bg-[#363057] border-t-1 border-[#3e3862]">
+        <Logo size={100} />
+      </div>
     </>
   );
 }
