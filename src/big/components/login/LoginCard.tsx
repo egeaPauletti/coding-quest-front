3a6aa922f3503b0d0e5d60e239d761773172271b
jmoranj <morandim.joao@gmail.com>
2025-04-13 23:40:13 -0300

    fix: Correct LoginCard flip logic and adjust Register section layout

--- src/big/components/login/LoginCard.tsx
+++ src/big/components/login/LoginCard.tsx
@@ -10,7 +10,6 @@ export default function LoginCard() {
 
   const handleFlip = () => {
     setIsFlipped(!isFlipped);
-    console.log(isFlipped);
   }
 
   return (
@@ -28,8 +27,8 @@ export default function LoginCard() {
       {isFlipped ?
 
         <div className="flex mx-auto w-[90%] max-w-[1200px] h-auto min-h-[600px] bg-[#fff] ring-[#3D3660] ring-2 shadow-2xl relative my-auto transition-all duration-500 ease-in-out">
-          <div className="bg-[url(./public/background/backgroundLoginCard.png)] justify-center text-center w-[60%]  pt-[10%] transition-all duration-500 ease-in-out">
 
+          <div className="bg-[url(./public/background/backgroundLoginCard.png)] justify-center text-center w-[60%] pt-[10%] transition-all duration-500 ease-in-out">
             <div className="flex-col text-center p-6">
               <h1 className="gradientText text-transparent bg-clip-text text-4xl font-bold font-">Bem Vindo!</h1>
 
@@ -45,7 +44,6 @@ export default function LoginCard() {
               <Button text="Entrar" height={45} width={150} onClick={handleFlip} />
             </div>
           </div>
-
 
           <div className="w-[100%] justify-center text-center pt-[9%] pb-[5%] transition-all duration-500 ease-in-out">
             <div className="flex-col text-center p-6">
@@ -63,22 +61,24 @@ export default function LoginCard() {
             <LoginForm />
           </div>
 
-
-          <div className="flex-col bg-[url(./public/background/backgroundLoginCard.png)] bg-cover pt-[10%] w-[60%] transition-all duration-500 ease-in-out">
+          <div className="flex-col bg-[url(./public/background/backgroundLoginCard.png)] bg-cover w-[60%] pt-[10%] transition-all duration-500 ease-in-out">
             <div className="flex-col text-center p-6">
-
               <h1 className="gradientText text-transparent bg-clip-text text-4xl font-bold font-">Bem Vindo!</h1>
+
               <p className="font-medium text-white pt-2">Ainda não tem uma conta no
-                <span className="gradientText text-transparent bg-clip-text mr-2"> CodingQuest? </span>
+                <span className="gradientText text-transparent bg-clip-text mr-4"> CodingQuest? </span>
                 Registre-se agora e venha fazer parte desse projeto!
               </p>
 
-              <div className="pt-25">
+              <div className="pt-30">
                 <Button text="Registre-se" height={45} width={150} onClick={handleFlip} />
               </div>
 
             </div>
           </div>
+
+
+
         </div>
 
       }
