a10b827e55fc6efe3804e32d8efdf9ba0af5f760
Gabriel Pauletti Egea <ga.paulettiegea@gmail.com>
2025-08-27 13:40:30 -0300

    reset: reset branch for project refactor

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import "./animations/animations.css";
+import "./index.css";
+//
+import { Route, Routes } from "react-router-dom";
+//
+//
+//
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element="" />
+      <Route path="/about" element="" />
+      <Route path="/login" element="" />
+      <Route path="/register" element="" />
+      <Route path="/home" element="" />
+      <Route path="/diaryChal" element="" />
+      <Route path="/campaign" element="" />
+      <Route path="/worlds/*" element="" />
+    </Routes>
+  );
+}
+
+export default App;
