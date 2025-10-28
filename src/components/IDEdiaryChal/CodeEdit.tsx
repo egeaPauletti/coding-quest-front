436b49f303dc8e5c193b72a3687e70d929743d3d
Gabriel Pauletti Egea <gah.paulettiegea@gmail.com>
2025-10-28 09:24:30 -0300

    fix(CodeEdit): standardize formatting and improve readability in the CodeEdit component

--- src/components/IDEdiaryChal/CodeEdit.tsx
+++ src/components/IDEdiaryChal/CodeEdit.tsx
@@ -1,5 +1,5 @@
 import { Editor } from "@monaco-editor/react";
-import { useState } from "react"; 
+import { useState } from "react";
 import {
   challenges,
   CODE_SNIPPETS,
@@ -11,21 +11,21 @@ import Output from "../IDE/Output";
 import SelectLanguage from "../IDE/SelectLanguage";
 
 const generateLastSevenDays = () => {
-    const days = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"];
-    const today = new Date();
-    const lastSevenDays = [];
+  const days = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"];
+  const today = new Date();
+  const lastSevenDays = [];
 
-    for (let i = 0; i < 7; i++) {
-        const date = new Date(today);
-        date.setDate(today.getDate() - i);
-        const dayOfWeek = days[date.getDay()];
-        const dayOfMonth = date.getDate();
-        
-        
-        lastSevenDays.unshift({ day: dayOfWeek, challengeCount: dayOfMonth, isToday: i === 0 });
-    }
+  for (let i = 0; i < 7; i++) {
+    const date = new Date(today);
+    date.setDate(today.getDate() - i);
+    const dayOfWeek = days[date.getDay()];
+    const dayOfMonth = date.getDate();
 
-    return lastSevenDays;
+
+    lastSevenDays.unshift({ day: dayOfWeek, challengeCount: dayOfMonth, isToday: i === 0 });
+  }
+
+  return lastSevenDays;
 };
 
 
@@ -37,12 +37,12 @@ const CodeEditor = () => {
   const [value, setValue] = useState(initialValue);
 
 
-  const id = 1; 
+  const id = 1;
 
   const worldChallenges = challenges.filter((c) => c.worldId === id);
 
   const WorldsData: WorldsData[] = WorldsDataSection;
-  
+
   const world = WorldsData.filter((c) => Number(c.id) === id);
   console.log(world);
 
@@ -52,7 +52,7 @@ const CodeEditor = () => {
     todayIndex !== -1 ? todayIndex : lastSevenDays.length - 1
 
   );
-  const dayOfMonth = lastSevenDays[selectedDayIndex]?.challengeCount ?? 1; 
+  const dayOfMonth = lastSevenDays[selectedDayIndex]?.challengeCount ?? 1;
   const currentChallenge = worldChallenges[(dayOfMonth - 1) % worldChallenges.length];
   const handleDayClick = (index: number) => {
     setSelectedDayIndex(index);
@@ -64,40 +64,38 @@ const CodeEditor = () => {
         <div className="flex w-full h-[15%] justify-between px-[1.5%]">
           <div className="flex flex-col justify-center">
             <span className="whiteColor opacity-50 text-sm 2xl:text-base">
-                Desafio do dia {lastSevenDays[selectedDayIndex]?.challengeCount ?? ""} |{" "}
-                O {world[0]?.worldName ?? "Mundo Desconhecido"}!
+              Desafio do dia {lastSevenDays[selectedDayIndex]?.challengeCount ?? ""} |{" "}
+              O {world[0]?.worldName ?? "Mundo Desconhecido"}!
             </span>
 
             <span className="whiteColor text-sm 2xl:text-base font-semibold">
-               Desafio: {currentChallenge?.desafio }
+              Desafio: {currentChallenge?.desafio}
             </span>
           </div>
-          <div className="flex gap-5 justify-center items-center">
-             <div className="flex items-center gap-4">
-                        <span className="whiteColor text-sm 2xl:text-base font-semibold">
-                            Desafios anteriores:
-                        </span>
-                        
-                       <div className="flex gap-2">
-                          {lastSevenDays.map((day, index) => (
-                               <div
-                                  key={index}
-                                  onClick={() => handleDayClick(index)}
-                                  className={`flex flex-col items-center justify-center p-2 rounded-lg 
-                                               ${day.isToday ? 'border-1 border-[#2ea98c] bg-black' : 'bg-black'} ${index === selectedDayIndex ? 'bg-gray-600' : ''} 
-                                               whiteColor text-xs 2xl:text-sm font-semibold cursor-pointer transition-colors hover:bg-gray-800`}
-                                              
-                                               style={{ width: '50px', height: '40px' }} 
-                                >
-                                   <span>{day.day}</span>
-                                  
-                                  <span>{day.challengeCount}</span> 
-                               </div>
-                           ))}
-                      </div>
-                   </div>
-            
+          <div className="flex gap-5 justify-center items-center pr-10">
+            <div className="flex items-center gap-4">
+              <span className="whiteColor text-sm 2xl:text-base font-semibold">
+                Desafios anteriores:
+              </span>
 
+              <div className="flex gap-2">
+                {lastSevenDays.map((day, index) => (
+                  <div
+                    key={index}
+                    onClick={() => handleDayClick(index)}
+                    className={`flex flex-col items-center justify-center rounded-sm 
+                                 ${index === selectedDayIndex ? 'border-1 border-[#2ea98c] bg-black' : 'bg-neutral-900'} 
+                                  whiteColor text-xs 2xl:text-sm font-semibold cursor-pointer transition-colors hover:bg-black/70`}
+
+                    style={{ width: '65px', height: '50px' }}
+                  >
+                    <span className="text-xs">{day.day}</span>
+
+                    <span>{day.challengeCount}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
         <div className="flex w-full justify-between items-center bg-[#181818] py-[1%] px-[1.5%]">
